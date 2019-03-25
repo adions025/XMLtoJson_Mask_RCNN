bb0210ff954bae3d429f537a8ea8b9d21fe48c12
Adonis González <adions025@gmail.com>
2019-03-25 13:36:27 +0100

    it works for only one annotation in xml

---
+++ dataset/indentation.py
@@ -0,0 +1,15 @@
+"""
+indentation
+
+To indentation json documents
+
+@author Adonis & Tom
+"""
+import json
+
+your_json = '{"IMG_20180413_091455.jpg": {"filename": "IMG_20180413_091455.jpg", "regions": {"0": {"region_attributes": {"damage": "damage"}, "shape_attributes": {"all_points_x": [2241, 2477, 2713, 2713, 2713, 2477, 2241, 2241, 2241], "all_points_y": [997, 997, 997, 1161, 1325, 1325, 1325, 1161, 997]}, "name": "polygon"}}, "size": 1231831}, "IMG_20180413_092043.jpg": {"filename": "IMG_20180413_092043.jpg", "regions": {"0": {"region_attributes": {"damage": "damage"}, "shape_attributes": {"all_points_x": [1307, 1361, 1415, 1415, 1415, 1361, 1307, 1307, 1307], "all_points_y": [2337, 2337, 2337, 2395, 2454, 2454, 2454, 2395, 2337]}, "name": "polygon"}}, "size": 1149276}}'
+parsed = json.loads(your_json)
+
+print(json.dumps(parsed, indent=4, sort_keys=True))
+
+   
