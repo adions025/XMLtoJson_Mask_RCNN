bf60d64565c2fd2f4066ca71cd4ef96bf8859d76
Adonis González <adions025@gmail.com>
2019-03-26 14:26:09 +0100

    some code improvements

--- dataset/convertXMLtoJSON.py
+++ dataset/convertXMLtoJSON.py
@@ -9,59 +9,49 @@ it works for one class in mask rcnn - damage class
 """
 
 import xml.etree.cElementTree as ET
-import cv2
-import os
 import json
 import sys
 import os
 
+#################################################
+#####                PATHS                  #####
+#################################################
 ROOT_DIR = os.path.dirname(os.path.realpath(__file__))
+path = []#list used for two folder /train and /val
+train = os.path.join(ROOT_DIR, "train")
+val = os.path.join(ROOT_DIR, "val")
+path = [train, val]
+count = 0
 
 def process_bar(count, total, status=''):
-    bar_len = 60
+    bar_len = 50
     filled_len = int(round(bar_len * count / float(total)))
     percents = round(100.0 * count / float(total), 1)
     bar = '=' * filled_len + '-' * (bar_len - filled_len)
-    sys.stdout.write('[%s] %s%s ...%s\r' % (bar, percents, '%', status))
-    sys.stdout.flush()
+    print(('[%s] %s%s ...%s\r' % (bar, percents, '%', status)))
 
 
 def grabNamesImages():
-    path=[]
-    train = os.path.join(ROOT_DIR, "train")
-    val = os.path.join(ROOT_DIR, "val")
-    path = [train,val]
     for file in path:
         files = os.listdir(file)
         for name in files:
-            imgs = []
+            #imgs = []
             with open(file + '/image.txt', 'w') as f:
                 for item in files:
                     if (item.endswith('.jpg')):
                         f.write("%s\n" % item)
             f.close()
-        print("List of images, images.txt, in", file)
+        print("List of images, images.tx, was save in", file)
 
 
 def XMLtoJson():
-    path = []
-    train = os.path.join(ROOT_DIR, "train")
-    val = os.path.join(ROOT_DIR, "val")
-    path = [train, val]
-
     for dir in path:
+        images, bndbox, size, polygon, all_json = {}, {}, {}, {}, {}
+
         imgs_list = open(dir+'/image.txt','r').readlines()
 
-        print("-----------")
-        print(imgs_list)
-        print("-----------")
-        images, bndbox, size, polygon = {}, {}, {}, {}
-        # images = []
         count = 1
         total = len(imgs_list)
-        all_json = {}
-
-
 
         for img in imgs_list: #for each image in the list in image.txt
             process_bar(count, total)
@@ -76,33 +66,20 @@ def XMLtoJson():
                 tree = ET.ElementTree(file=dir+'/'+xml_n)
                 root = tree.getroot()
 
-                #dictorionais
-                counterObject = {}
-                xmin = {}
-                xmax = {}
-                ymin = {}
-                ymax = {}
-                regionsTemp = {}
-
-                regi = {}
+                #more dics for create json documents
+                counterObject,xmin,xmax,ymin,ymax,regionsTemp,regi = {},{},{},{},{},{},{}
 
                 number = 0
                 for child_of_root in root:
                     if child_of_root.tag == 'filename':
                         image_id = (child_of_root.text)
-
                         sizetmp = os.path.getsize(dir+'/'+image_id)
 
                     if child_of_root.tag == 'object':
-                        #number=0
-
-
                         for child_of_object in child_of_root:
-
                             if child_of_object.tag == 'name':
                                 category_id = child_of_object.text
                                 counterObject[category_id] = number
-
 
                             if child_of_object.tag == 'bndbox':
                                 for child_of_root in child_of_object:
@@ -118,11 +95,8 @@ def XMLtoJson():
                                     if child_of_root.tag == 'ymax':
                                         ymax[category_id] = int(child_of_root.text)
 
-
-
                         xmintmp = int(xmax[category_id] - xmin[category_id]) / 2
                         xvalue = int(xmin[category_id] + xmintmp)
-
 
                         ymintemp = int(ymax[category_id] - ymin[category_id]) / 2
                         yvalue = int(ymin[category_id] + ymintemp)
@@ -131,10 +105,8 @@ def XMLtoJson():
                         regionsTemp = ({"all_points_x": (xmin[category_id], xvalue, xmax[category_id], xmax[category_id], xmax[category_id], xvalue, xmin[category_id], xmin[category_id], xmin[category_id]),
                                         "all_points_y": (ymin[category_id], ymin[category_id], ymin[category_id], yvalue, ymax[category_id], ymax[category_id], ymax[category_id], yvalue, ymin[category_id])})
 
-                        #print(regionsTemp[category_id])
                         damage = {"damage": "damage"}
                         regions.update({"region_attributes": damage})
-
 
                         shapes = {"shape_attributes": regionsTemp}
 
@@ -143,8 +115,6 @@ def XMLtoJson():
                         polygon.update({"name": "polygon"})
                         regions.update(shapes)
                         regions.update(polygon)
-
-
 
                         regi[number] = regions.copy()
                         #print(regi[number],[category_id])
@@ -156,23 +126,12 @@ def XMLtoJson():
                         images.update(size)
 
                         all_json[img_name] = images.copy()
-
-                        print(all_json)
-
-                        print(number)
-
                         number = number + 1
-
-
-
 
         with open(dir+'/'+"dataset.json", "a") as outfile:
             json.dump(all_json, outfile)
             print("File dataset.json was save in: ",dir )
-
             open(dir+'/'+"dataset.json", "w").close()
-
-
 
 
 if __name__ == "__main__":
