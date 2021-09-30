d03444a757242b98e04932c2d7d8e65831a53733
Adonis Gonzalez <adions025@gmail.com>
2021-09-30 12:38:59 +0200

    updated!

--- src/main.py
+++ src/main.py
@@ -7,82 +7,15 @@ converts to json (polygon shape).
 @author Adonis Gonzalez
 
 """
-from os.path import join, dirname, realpath, exists, isfile
+from os.path import dirname, realpath
 import xml.etree.cElementTree as et
-from os import listdir
 from src.util import *
 import json
-import os
 
 ROOT_DIR = dirname(realpath(__file__))
 data_dir = join(ROOT_DIR, "../dataset")
 train_dir = join(data_dir, "train")
 val_dir = join(data_dir, "val")
-
-
-def has_files(path: str) -> bool:
-    """
-    Checks if there is any file in the given path.
-    If not files, is not possible continue.
-
-    :param path: A str like /to/path/
-    :return: A bool True/False
-    """
-    assert exists(path), "--path is not correct"
-    return any([j for j in listdir(path) if j.endswith('.jpg')])
-
-
-def list_images(path: str) -> list:
-    """
-    Return a list of files that ends with .txt extension
-
-    :param path: A str like /to/path/
-    :return: A list that contains all found files
-    """
-    assert exists(path), "--path is not correct"
-    return [j for j in listdir(path) if j.endswith('.jpg')]
-
-
-def save_images_log(path: str) -> list:
-    """
-    Grab images list and save a log in a specific path.
-
-    :param path: A str like /to/path/
-    """
-    assert exists(path), "--path is not correct"
-    if not has_files(path):
-        print("Not images found, try using other path or adding images")
-        exit(0)  # If not images, exit, cause it is necessary to have xml and jpg together
-    else:
-        images = list_images(path)
-        f = open(join(path, "image.txt"), 'w')
-        [f.write("%s\n" % x) for x in images]
-        f.close()
-        return images
-
-
-def read_json(dir_path: str, filename: str) -> json:
-    """
-    To read json files
-
-    :param dir_path: A str like /to/path/
-    :param filename: A str, json file name e.g data.json
-    :return: A JSON file object
-    """
-    file = join(dir_path, filename)
-    assert isfile(file), "-- check your path file"
-    return json.load(open(file))
-
-
-def remove_file(file_name: str):
-    """
-    Remove a given file, if not raises assert.
-
-    :param file_name: A str like /to/path/file
-    """
-    assert isfile(file_name), "-- check your path file"
-    os.remove(file_name)
-    print("Deleting file %s" % file_name)
 
 
 def convert_xml_to_json(path: str, image_list: list):
@@ -137,12 +70,11 @@ def convert_xml_to_json(path: str, image_list: list):
 
 
 if __name__ == "__main__":
-    # Json file for both train and val dir
+    # I convert both train and val dataset annotation
     file_train = join(train_dir, "dataset.json")
     file_val = join(val_dir, "dataset.json")
 
     # Remove if exist dataset.json in both train and val
-    # Just to create a new files
     remove_file(file_train)
     remove_file(file_val)
 
@@ -154,7 +86,7 @@ if __name__ == "__main__":
     convert_xml_to_json(train_dir, images_train)
     convert_xml_to_json(val_dir, images_val)
 
-    json1 = json.dumps(read_json(train_dir, "dataset.json"), sort_keys=True)
-    json2 = json.dumps(read_json(train_dir, "dataset_good.json"), sort_keys=True)
-    if json1 == json2:
-        print("Equals!")
+    # json1 = json.dumps(read_json(train_dir, "dataset.json"), sort_keys=True)
+    # json2 = json.dumps(read_json(train_dir, "dataset_good.json"), sort_keys=True)
+    # if json1 == json2:
+    #     print("Equals!")
--- src/util.py
+++ src/util.py
@@ -1,7 +1,12 @@
 """
 Util functions
 """
+
 from typing import Tuple
+from os.path import join, exists, isfile
+from os import listdir
+import json
+import os
 
 
 def calculate_xy(x_max: int, x_min: int, y_max: int, y_min: int) -> Tuple[int, int]:
@@ -34,3 +39,68 @@ def get_points(x_max: int, x_min: int, y_max: int, y_min: int, x_value: int, y_v
          "all_points_y": (y_min, y_min, y_min, y_value, y_max, y_max, y_max, y_value, y_min)
          })
     return regions
+
+
+def has_files(path: str) -> bool:
+    """
+    Checks if there is any file in the given path.
+    If not files, is not possible continue.
+
+    :param path: A str like /to/path/
+    :return: A bool True/False
+    """
+    assert exists(path), "--path is not correct"
+    return any([j for j in listdir(path) if j.endswith('.jpg')])
+
+
+def list_images(path: str) -> list:
+    """
+    Return a list of files that ends with .txt extension
+
+    :param path: A str like /to/path/
+    :return: A list that contains all found files
+    """
+    assert exists(path), "--path is not correct"
+    return [j for j in listdir(path) if j.endswith('.jpg')]
+
+
+def save_images_log(path: str) -> list:
+    """
+    Grab images list and save a log in a specific path.
+
+    :param path: A str like /to/path/
+    """
+    assert exists(path), "--path is not correct"
+    if not has_files(path):
+        print("Not images found, try using other path or adding images")
+        exit(0)  # If not images, exit, cause it is necessary to have xml and jpg together
+    else:
+        images = list_images(path)
+        f = open(join(path, "image.txt"), 'w')
+        [f.write("%s\n" % x) for x in images]
+        f.close()
+        return images
+
+
+def read_json(dir_path: str, filename: str) -> json:
+    """
+    To read json files
+
+    :param dir_path: A str like /to/path/
+    :param filename: A str, json file name e.g data.json
+    :return: A JSON file object
+    """
+    file = join(dir_path, filename)
+    assert isfile(file), "-- check your path file"
+    return json.load(open(file))
+
+
+def remove_file(file_name: str):
+    """
+    Remove a given file, if not raises assert.
+
+    :param file_name: A str like /to/path/file
+    """
+    assert isfile(file_name), "-- check your path file"
+    os.remove(file_name)
+    print("Deleting file %s" % file_name)
